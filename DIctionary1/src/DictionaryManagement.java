c19b6ddf259e5986c210f17f4814dec8e8fda725
Nguyen Duc An - 19020202 <58422724+ducan3007@users.noreply.github.com>
2020-10-02 22:15:20 +0700

    Update DictionaryManagement.java

--- DIctionary1/src/DictionaryManagement.java
+++ DIctionary1/src/DictionaryManagement.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
@@ -6,6 +9,7 @@ import java.util.Scanner;
 public class DictionaryManagement {
     static Scanner input = new Scanner(System.in);
     static Dictionary dictionary = new Dictionary();
+    static DictionaryCommandLine dictionaryCommandLine = new DictionaryCommandLine();
 
     public static void insertFromCommandLine() {
         System.out.println("Nhap so luong tu : ");
@@ -19,5 +23,32 @@ public class DictionaryManagement {
             dictionary.getListWords().add(temp);
         }
         dictionary.getListWords().sort((o1, o2) -> o1.getWord().compareTo(o2.getWord()));
+    }
+    public static void insertFromFile() {
+        try {
+            Scanner in = new Scanner(new BufferedReader(new FileReader("Dictionary1/src/dictionaries.txt")));
+            while (in.hasNext()) {
+                String temp = in.nextLine().replaceAll("\\s+", " ");
+                for (int i = 0; i < temp.length(); i++) {
+                    if (temp.charAt(i) == ' ') {
+                        Word w = new Word(temp.substring(0, i), temp.substring(i + 1, temp.length()));
+                        dictionary.getListWords().add(w);
+                        break;
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+        }
+    }
+    public static void dictionaryLookup() {
+        System.out.println("Nhap tu ban muon tra: ");
+        Scanner in = new Scanner(System.in);
+        String word = in.nextLine();
+        int index = dictionary.isContain(word);
+        if (index >= 0) {
+            System.out.println("Nghĩa của từ " + "(" + dictionary.getListWords().get(index).getWord() + ") " + "la : " + dictionary.getListWords().get(index).getExplain());
+        } else {
+            System.out.println("Từ này không có trong từ điển!");
+        }
     }
 }
